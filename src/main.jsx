edf43af40a0fa3983055c4f5fdebd6b2392b7de4
John <batsikadzejoni@gmail.com>
2025-12-01 15:25:19 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,17 @@
-// src/main.jsx
 
-import React, { Suspense } from 'react'; // 1. Импортируем Suspense
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-
-// (ThemeProvider тут уже есть, если ты его оставил)
-// import { ThemeProvider } from './contexts/ThemeContext.jsx'
-
-// 2. ИМПОРТИРУЕМ наш "мозг" i18n
 import './i18n';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/*
-      3. Оборачиваем App в <Suspense>.
-      Это ОБЯЗАТЕЛЬНО. Так как i18n загружает JSON (переводы) асинхронно,
-      React должен "подождать", пока они загрузятся.
+      Suspense
     */}
     <Suspense fallback="...Loading">
-      {/* <ThemeProvider> (если у тебя есть, он остается ВНУТРИ Suspense) */}
+      {/* <ThemeProvider>*/}
         <App />
       {/* </ThemeProvider> */}
     </Suspense>
